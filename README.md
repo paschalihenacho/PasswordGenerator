5126e820432e41a109c5abbabf83e265739233f4
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-04-23 18:40:43 -0500

    --- Auto-Git Commit ---

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PasswordGenerator
+This application generates password based on user selected options.
+
+# Credits:
+1. https://owasp.org/www-community/password-special-characters
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Arrays
+// Arrays of choices
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
                         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
