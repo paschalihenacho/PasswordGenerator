8a81cd9631e77d1473f4664fa325a55a2d30ce74
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-04-26 12:59:34 -0500

    --- Auto-Git Commit ---

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.querySelector("#generate").addEventListener("click", writePassword);
-// Arrays of choices
+
+var confirmLength = "";
+var confirmSpecialChar;
+var confirmNumericChar;
+var confirmUpperCase;
+var confirmLowerCase;
+
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
                         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
@@ -7,29 +12,16 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["/", "&", ".", "@","!","~", "-", "*", "%", ">", "+", ",", "<", "?"];
 
-// var confirmLength = "";
-// var upperCase;
-// var lowerCase;
-// var confirmSpecialChar;    
-// var confirmNumeric;
-var confirmLength = "";
-var confirmSpecialChar;
-var confirmNumericChar;
-var confirmUpperCase;
-var confirmLowerCase;
-
-// Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  var confirmLength = (prompt("How long do you want your password to be?"));
 
-  // Loop if answer is outside the parameters 
-  if(confirmLength <= 8 || confirmLength >= 100) {
-      alert("Password length must be between 8-50 characters Try again");
-      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  while(confirmLength <= 7 || confirmLength >= 128) {
+      alert("Please input the length between 8 to 128 characters.");
+      var confirmLength = (prompt("How long do you want your password to be?"));
       } 
 
-      // Repeat back how many charactes the user will have  
-alert(`Your password will have ${confirmLength} characters`);
+    //confirm user password length input
+      alert("Your password inludes " + confirmLength + " characters");
 
     // Determine parameters of password 
     var confirmSpecialChar = confirm("Click OK to confirm if you would like to include special characters");
@@ -83,3 +75,4 @@ function writePassword() {
 
   passwordText.value = password;
 }
+document.querySelector("#generate").addEventListener("click", writePassword);
