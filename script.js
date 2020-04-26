98089ddc010b92262944bf028143bf6b0c451ed6
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-04-26 13:29:42 -0500

    --- Auto-Git Commit ---

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 var confirmLength = "";
-var confirmSpecialChar;
-var confirmNumericChar;
 var confirmUpperCase;
 var confirmLowerCase;
+var confirmSpecialChar;
+var confirmNumericChar;
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
                         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -20,25 +20,25 @@ function generatePassword() {
       var confirmLength = (prompt("How long do you want your password to be?"));
       } 
 
-    //confirm user password length input
       alert("Your password inludes " + confirmLength + " characters");
 
-    // Determine parameters of password 
-    var confirmSpecialChar = confirm("Click OK to confirm if you would like to include special characters");
-    var confirmNumericChar = confirm("Click OK to confirm if you would like to include numeric characters");    
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+    var confirmSpecialChar = confirm("Click OK to include special characters");
+    var confirmNumericChar = confirm("Click OK to include numeric");    
+    var confirmLowerCase = confirm("Click OK to include lowercase letters");
+    var confirmUpperCase = confirm("Click OK to include uppercase characters");
       // Loop if answer is outside the parameters 
-      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialChar === false && confirmNumericChar === false) {
-        alert("You must choose at least one parameter");
-        var confirmSpecialChar = confirm("Click OK to confirm if you would like to include special characters");
-        var confirmNumericChar = confirm("Click OK to confirm if you would like to include numeric characters");    
-        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
+      while(confirmUpperCase !== true && confirmLowerCase !== true && confirmSpecialChar !== true && confirmNumericChar !== true) {
+        alert("You must choose at least one character type");
+        var confirmSpecialChar = confirm("Click OK to include special characters");
+        var confirmNumericChar = confirm("Click OK to include numeric");    
+        var confirmLowerCase = confirm("Click OK include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to include uppercase characters");   
     } 
 
-      // Assign an action to the password parameters NEED TO FIX THIS
       var passwordCharacters = []
+      if (confirmUpperCase) {
+        passwordCharacters = passwordCharacters.concat(upperCase)
+      }
       
     if (confirmSpecialChar) {
       passwordCharacters = passwordCharacters.concat(specialChar)
@@ -52,9 +52,7 @@ function generatePassword() {
       passwordCharacters = passwordCharacters.concat(lowerCase)
     }
 
-    if (confirmUpperCase) {
-      passwordCharacters = passwordCharacters.concat(upperCase)
-    }
+
 
       console.log(passwordCharacters)
 
