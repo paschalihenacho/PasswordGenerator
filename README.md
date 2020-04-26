550cf9a09e22412719cf721333524e41b8608961
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-04-25 23:59:49 -0500

    a While loop to check password length

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PasswordGenerator
 This application generates password based on user selected options.
+the password string can range from 8 to 100 characters.
+
+User can input the password lenth at minimum of 8 characters and maximum of 100 characters.
 
 # Credits:
 1. https://owasp.org/www-community/password-special-characters
--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 // Arrays of choices
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
                         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
                         "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["/", "&", ".", "@","!","~", "-", "*", "%", ">", "+", ",", "<", "?"];
 
 var confirmLength = "";
-var upperCaseLetters;
-var lowerCaseLetters;
+var upperCase;
+var lowerCase;
 var confirmSpecialCharacter;
-var confirmNumericCharacter;
+var confirmNumeric;
 
-
-alert("This application gives password with 8-50 characters according to user input.");
 
 function generatPassword() {
   var confirmLength = prompt("How long do you want your password to be?")
 }
+
+// a while loop to ensure user password lenth option is within the required range 
+while(confirmLength <= 8 || confirmLength >= 100) {
+  alert("Please input the length between 8 to 100 characters.");
+  var confirmLength = (prompt("How long do you want your password to be?"));
+  } 
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
